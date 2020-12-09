00c72b5a618a09307b80e22ba4bc69e577f88188
Carlos Lomeli <sackrone@hotmail.com>
2020-12-09 14:00:26 -0600

    15.1

---
+++ FootBall.Web/Helpers/CombosHelper.cs
@@ -0,0 +1,22 @@
+﻿using FootBall.Web.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+
+namespace FootBall.Web.Helpers
+{
+    public class CombosHelper : ICombosHelper
+    {
+        private readonly DataContext _context;
+
+        public CombosHelper(DataContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<SelectListItem> GetComboClubs()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FootBall.Web/Helpers/ICombosHelper.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace FootBall.Web.Helpers
+{
+    public interface ICombosHelper
+    {
+        IEnumerable<SelectListItem> GetComboClubs();
+    }
+}
